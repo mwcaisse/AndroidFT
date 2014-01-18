3763625702baefbf924de2582e5bea965f8b033d
Mitchell Caisse <computers1055@gmail.com>
2014-01-18 12:13:54 -0500

    Added xml configuration files for the servlet

--- aft-serlet/src/main/java/com/ricex/aft/servlet/controller/DeviceController.java
+++ aft-serlet/src/main/java/com/ricex/aft/servlet/controller/DeviceController.java
@@ -1,6 +1,7 @@
 package com.ricex.aft.servlet.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Mitchell Caisse
@@ -9,5 +10,10 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class DeviceController {
+	
+	@RequestMapping("/device")
+	public String deviceTest() {
+		return "Hello World";
+	}
 
 }
