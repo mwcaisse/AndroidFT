bf22675fdaee1753ff37bde7412fe386121d35ca
Mitchell Caisse <computers1055@gmail.com>
2014-01-19 21:01:44 -0500

    Defined the necessary methods in Device Mapper interface

--- aft-servlet/src/main/java/com/ricex/aft/servlet/mapper/DeviceMapper.java
+++ aft-servlet/src/main/java/com/ricex/aft/servlet/mapper/DeviceMapper.java
@@ -3,6 +3,10 @@
  */
 package com.ricex.aft.servlet.mapper;
 
+import java.util.List;
+
+import com.ricex.aft.servlet.entity.Device;
+
 /** MyBatis Mapper for devices
  * 
  * @author Mitchell Caisse
@@ -10,4 +14,23 @@ package com.ricex.aft.servlet.mapper;
  */
 public interface DeviceMapper {
 
+	/** Returns a list of all devices
+	 * 
+	 * @return list of devices
+	 */
+	public List<Device> getAllDevices();
+	
+	/** Updates the device
+	 * 
+	 * @param device The device to update, uses the unique id to update
+	 */
+	
+	public void updateDevice(Device device);
+	
+	/** Creates the given device. Will assign the device a new id
+	 * 
+	 * @param device The device to create
+	 */
+	
+	public void createDevice(Device device);
 }
