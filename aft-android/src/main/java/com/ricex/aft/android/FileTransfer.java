db17708c4199a94457eb47f65c2a16d254821107
Mitchell Caisse <computers1055@gmail.com>
2014-01-27 11:32:25 -0500

    Added permissions needed for android, and updated the pom to include the jar dep

--- aft-android/src/main/java/com/ricex/aft/android/FileTransfer.java
+++ aft-android/src/main/java/com/ricex/aft/android/FileTransfer.java
@@ -1,8 +1,11 @@
 package com.ricex.aft.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 
 public class FileTransfer extends Activity {
 
@@ -16,6 +19,17 @@ public class FileTransfer extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.file_transfer, menu);
+		return true;
+	}
+	
+	/** Check if the device currently has google play services installed
+	 * 
+	 * @return
+	 */
+	
+	public boolean checkGooglePlayServices() {
+		int res = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+		
 		return true;
 	}
 
