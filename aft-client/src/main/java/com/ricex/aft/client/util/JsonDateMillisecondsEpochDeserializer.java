ca32735566991e0491c7e91afcc77e2961428375
Mitchell Caisse <computers1055@gmail.com>
2014-05-09 15:22:23 -0400

    Updated the JSON Parser to serialize Date as milliseconds since epoch

--- aft-client/src/main/java/com/ricex/aft/client/util/JsonDateMillisecondsEpochDeserializer.java
+++ aft-client/src/main/java/com/ricex/aft/client/util/JsonDateMillisecondsEpochDeserializer.java
@@ -10,6 +10,8 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
 
 /**
  * @author Mitchell Caisse
@@ -24,6 +26,10 @@ public class JsonDateMillisecondsEpochDeserializer implements JsonDeserializer<D
 	public Date deserialize(JsonElement json, Type typeOfT,	JsonDeserializationContext context) 
 			throws JsonParseException {
 		return new Date(json.getAsJsonPrimitive().getAsLong());
+	}
+	//
+	public JsonElement serialize(Date source, Type type, JsonSerializationContext context) {
+		return new JsonPrimitive(source.getTime());
 	}
 }
 
