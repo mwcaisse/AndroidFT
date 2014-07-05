eae97ee0db9075c27f2b0fd83367aa37754d2e95
Mitchell Caisse <computers1055@gmail.com>
2014-07-05 13:30:49 -0400

    Implementation of Issue #23, Proper log on using Digest Authentication

--- aft-servlet/src/main/java/com/ricex/aft/servlet/config/SecurityConfig.java
+++ aft-servlet/src/main/java/com/ricex/aft/servlet/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.ricex.aft.servlet.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
 
 /** Java Config for Spring Security
  * 
@@ -18,7 +22,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.exceptionHandling().authenticationEntryPoint(digestAuthenticationEntryPoint()).and()
+			.authorizeRequests().antMatchers("/manager/**").hasRole("USER")
+			.anyRequest().authenticated()
+			.and().addFilter(digestAuthenticationFilter());
+			
+	}
+	
+	public DigestAuthenticationFilter digestAuthenticationFilter() {
+		DigestAuthenticationFilter filter = new DigestAuthenticationFilter();
+		filter.setAuthenticationEntryPoint(digestAuthenticationEntryPoint());
+		filter.setUserDetailsService(super.userDetailsService());
+		return filter;
+	}
+	
+	@Bean
+	public DigestAuthenticationEntryPoint digestAuthenticationEntryPoint() {
+		DigestAuthenticationEntryPoint entryPoint = new DigestAuthenticationEntryPoint();
+		entryPoint.setKey("ricex1055AFT");
+		entryPoint.setRealmName("AFT");
+		return entryPoint;
 	}
 }
