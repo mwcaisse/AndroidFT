3db86b2a3218cd310ba860cbabf19e66c3f5bb14
Mitchell Caisse <computers1055@gmail.com>
2014-08-10 12:39:24 -0400

    Added a view for displaying a list of all requests

---
+++ aft-web/web/js/ViewModels/Request/PFRequestListViewModel.js
@@ -0,0 +1,56 @@
+
+/** The model for a Request */
+function RequestModel(data) {	
+	var self = this;	
+	if (data) {		
+		/** The id of this request */
+		self.requestId = data.requestId;
+		
+		/** The name of this request */
+		self.requestName = data.requestName;
+		
+		/** The status of this request */
+		self.requestStatus = data.requestStatus;
+		
+		/** The date that this request was updated */
+		self.requestUpdated = data.requestUpdated;
+		
+		self.requestUpdatedText = ko.computed( function() {
+			return parseDate(self.requestUpdated);
+		});
+		
+		/** The name of the device this request belongs to */
+		self.requestDeviceName = data.requestDevice.deviceName;		
+	}
+	
+}
+
+
+/** The view model for displaying a list of all requests
+ * 
+ */
+function PFRequestListViewModel() {
+
+	var self = this;
+	
+	/** The list of requests to show */
+	self.Requests = ko.observableArray([]);
+	
+	/** Fetches the requests from the server */
+	self.fetchRequests = function() {
+		$.getJSON("http://" + host + "/aft-servlet/manager/request/all", function(data) {	
+			//remove the old requests, and add the new requests
+			self.Requests.removeAll();			
+			$.each(data, function(index, value) {
+				self.Requests.push(new RequestModel(value));
+			});			
+		}).fail( function(jqXHR, textStatus, error) {
+			alert("Error fethcing requests! " + textStatus + " : " + error);
+		});		
+	};
+	
+	
+	//fetch the requests
+	self.fetchRequests();
+	
+}
