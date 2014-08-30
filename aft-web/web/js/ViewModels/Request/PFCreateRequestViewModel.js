4ecf5b2709f747271d4f3f5cb8e3c4d6c635684b
Mitchell Caisse <computers1055@gmail.com>
2014-08-29 22:15:48 -0400

    Updated Create Request, now supports saving requests to the server

--- aft-web/web/js/ViewModels/Request/PFCreateRequestViewModel.js
+++ aft-web/web/js/ViewModels/Request/PFCreateRequestViewModel.js
@@ -13,6 +13,7 @@ function RequestModel(data) {
 	self.requestStatusMessage = ko.observable("");
 	self.requestDevice = ko.observable(new DeviceModel());
 	self.requestFiles = ko.observableArray([]);
+	self.requestUpdated = ko.observable(new Date());
 	
 	//if there is data initialize the data..
 	if (data) {
@@ -22,12 +23,80 @@ function RequestModel(data) {
 		self.requestStatus(data.requestStatus);
 		self.requestStatusMessage(data.requestStatusMessage);
 		self.requestDevice(new DeviceModel(data.requestDevice));
+		self.requestUpdated = ko.observable(new Date(self.requestUpdate));
 		
 		//add the files
 		$.each(data.requestFiles, function( index, value) {
 			self.requestFiles.push(new FileModel(value));
 		});
 	}
+	
+	/** Whether or not this request is new */
+	self.isNew = ko.computed(function() {
+		return self.requestId < 0; //if the id is less than zero, it is new
+	});
+	
+	self.statusTemplate = ko.computed(function() {
+		if (self.isNew()) {
+			//if it is new status is non-modifiable
+			return "request-status-non-modifiable";
+		}
+		else {
+			//otherwise return modifiable
+			return "request-status-modifiable";
+		}
+	});
+	
+	
+	/** The error for the request */
+	self.requestError = ko.observable(false);
+	self.requestErrorText = ko.observable("");
+	
+	/** The error for the request files */
+	self.requestFilesError = ko.observable(false);
+	self.requestFilesErrorText = ko.observable("");
+	
+	/** Determines if the request is valid
+	 * 	@return True if the request is valid, false otherwise
+	 */
+	self.validate = function() {
+		//check if the request has a name
+		if (self.requestName().length <= 0) {
+			self.requestError(true);
+			self.requestErrorText("A request must have a name!");
+		}
+		else {
+			self.requestError(false);
+			self.requestErrorText("");
+		}
+		
+		//check if the request has files
+		if (self.requestFiles().length < 1) {
+			self.requestFilesError(true);
+			self.requestFilesErrorText("A request must have atleast one file!");
+		}
+		else {
+			self.requestFilesError(false);
+			self.requestFilesErrorText("");
+		}
+		
+		//if both errors are false, then we are valid, otherwise invalid
+		return !self.requestFilesError() && !self.requestError();
+	};
+	
+	//subscribe to the name and request files to validate the request
+	self.requestName.subscribe(self.validate);
+	self.requestFiles.subscribe(self.validate);
+	
+	/** Removes the specified file
+	 * 	@param The file to remove
+	 */
+	self.removeRequestFile = function(file) {
+		self.requestFiles.remove(file);
+	};
+	
+	//validate the request
+	self.validate();	
 	
 }
 
@@ -52,6 +121,15 @@ function FileModel(data) {
 	
 	self.fileId = data.fileId;
 	self.fileName = data.fileName;
+	
+	/** The url to download the file
+	 */
+	self.downloadUrl = ko.computed(function() {
+		if (self.fileId < 0) {
+			return "#"; //no id, can't download
+		}
+		return "http://" + host + "/aft-servlet/manager/file/download/" + self.fileId;
+	});
 	
 }
 
@@ -106,11 +184,70 @@ function PFCreateRequestViewModel(fileUploadModal) {
 		});
 	};
 	
+	/** Fetches the request */
+	self.fetchRequest = function() {
+		//if the request is new, clear out the current values, otherwise fetch a new request
+		if (self.request().isNew()) {
+			self.request(new RequestModel());
+		}
+		else {
+			$.getJSON("http://" + host + "/aft-servlet/manager/request/" + self.request().requestId, function (data) {
+				self.request(new RequestModel(data));
+			}).fail( function (jqXHR, textStatus, error) {
+				alert("Failed to save request: " + textStatus + " : " + error);
+			});
+		}
+	};
+	
 	/** Fetches all required data from the server */
 	self.fetchData = function() {
 		self.fetchRequestStatuses();
 		self.fetchRequestDirectories();
 		self.fetchDevices();
+	};
+
+	
+	/** Saves the request
+	 * 
+	 */
+	self.saveRequest = function() {
+		if (self.request().validate()) {			
+			//The url to make the request to
+			var url = "";
+			//the type of request to make
+			var type= "POST";
+			
+			//set the url and type depending on whether it is a creation or update
+			if (self.request().isNew()) {
+				url = "http://" + host + "/aft-servlet/manager/request/create";
+				type =  "POST";
+			}
+			else {
+				url = "http://" + host + "/aft-servlet/manager/request/update";
+				type =  "PUT";
+			}			
+			$.ajax( {
+            	url: url,
+            	type: type,
+            	data: ko.toJSON(self.request()),
+            	contentType: "application/json"             	
+            }).done( function(data) {
+            	//update the request id
+            	self.request().requestId = data.requestId;
+            }).fail( function (jqXHR, textStatus, error) {
+            	alert("Failed to save the request! : " + textStatus + " : " + error);
+            });
+		}
+		else {
+			alert("Please fix any issues with the request before saving");
+		}
+	};
+	
+	/** Discards / refreshes any data
+	 * 
+	 */
+	self.discard = function() {
+		self.fetchData();
 	};
 	
 	/** The callback for uploading request files */
@@ -128,6 +265,16 @@ function PFCreateRequestViewModel(fileUploadModal) {
 	 */
 	self.addRequestFile = function() {
 		showFileUploadModal(self.requestFileCallback);
+	};
+	
+	/** Removes the specified file, or the calling context if no param is passed
+	 * 	@param The file to remove, optional
+	 */
+	self.removeRequestFile = function(file) {
+		if (file == null) {
+			file = this;
+		}
+		self.request().removeRequestFile(file);
 	};
 	
 	self.fetchData();
