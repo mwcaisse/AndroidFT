e4ccd53da0b2103aee0a02604a58be743d3cf4ed
Mitchell Caisse <computers1055@gmail.com>
2014-11-14 21:43:33 -0500

    Enabled Logging out + Added a log out button

--- aft-servlet/src/main/java/com/ricex/aft/servlet/config/SecurityConfig.java
+++ aft-servlet/src/main/java/com/ricex/aft/servlet/config/SecurityConfig.java
@@ -10,8 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -46,7 +45,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.formLogin()
 				.loginPage("/login")
-				.permitAll();
+				.permitAll()
+				.and()
+			.logout()
+				.invalidateHttpSession(true)
+				.logoutSuccessUrl("/login?logout")
+				.logoutUrl("/logout")
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
 			
 	}	
 	
