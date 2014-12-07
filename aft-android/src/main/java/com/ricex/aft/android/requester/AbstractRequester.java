e98d2486f735b5ac6dd4fb9aec458104dff9f120
Mitchell Caisse <computers1055@gmail.com>
2014-12-07 18:54:07 -0500

    Updated the android depdenencies to latest AFT Common + Milestone release of spring-android. Started adding support for the AFT AUthentication

--- aft-android/src/main/java/com/ricex/aft/android/requester/AbstractRequester.java
+++ aft-android/src/main/java/com/ricex/aft/android/requester/AbstractRequester.java
@@ -6,6 +6,12 @@ package com.ricex.aft.android.requester;
 import java.text.DateFormat;
 import java.util.Date;
 
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +21,7 @@ import android.provider.Settings;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.ricex.aft.android.util.AndroidJsonByteArrayBase64Adapter;
+import com.ricex.aft.common.auth.AFTAuthentication;
 import com.ricex.aft.common.util.JsonDateMillisecondsEpochDeserializer;
 
 /**
@@ -31,6 +38,9 @@ public abstract class AbstractRequester {
 	
 	/** The server address */
 	protected final String serverAddress;
+	
+	/** The current Authentication Token */
+	private String aftToken;
 	
 	/** Creates a new Abstract requestor with the specified context
 	 * 
@@ -55,6 +65,7 @@ public abstract class AbstractRequester {
 		
 		//add the gson message converter to the rest template
 		restTemplate.getMessageConverters().add(converter);
+				
 	}
 	
 	/** Returns the UID for the device this app is running on
@@ -66,5 +77,99 @@ public abstract class AbstractRequester {
 		String androidId = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
 		return androidId;
 	}	
+	
+	protected <T> T makeRequest(String url, HttpMethod method, HttpEntity<?> requestEntity, ParameterizedTypeReference<T> responseType, Object... urlVariables) {	
+		HttpEntity<?> entity = addAuthenticationHeaders(requestEntity);
+		ResponseEntity<T> results = restTemplate.exchange(url, method, entity, responseType, urlVariables);		
+		return processRequestResponse(results, url, method, requestEntity, responseType, urlVariables);
+	}
+	
+	//TODO: Make an AFTRequest object
+	
+	/** Processes the results of an AFT Request 
+	 * 
+	 * @param responseEntity The results of the request 
+	 * @param url The url of the request
+	 * @param method The method of the request
+	 * @param requestEntity The response entity of the request
+	 * @param responseType the response type of the request
+	 * @param urlVariables The url variables of the request
+	 * @return The processed results of the request, the request body or null if there was an error
+	 */
+	protected <T> T processRequestResponse(ResponseEntity<T> responseEntity, String url, HttpMethod method, HttpEntity<?> requestEntity, 
+			ParameterizedTypeReference<T> responseType, Object... urlVariables) {
+		
+		T res = null;
+		//check the status of the response
+		if (responseEntity.getStatusCode() == HttpStatus.OK) {
+			res = responseEntity.getBody();
+			//if we need authentication token, extract it from the response
+			if (needAuthenticationToken()) {
+				extractAuthenticationToken(responseEntity);
+			}
+		}
+		else if (responseEntity.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+			if (needAuthenticationToken()) {
+				//401 was returned, and we still need a token
+				//TODO: means invalid credentials, raise this up somehow
+				res = null; // for now we return null
+			}
+			else {
+				//401 was returned, and we don't need a token,
+				// our token has most likely expired, invalidate our current token, and re-request
+				invalidateAFTToken();
+				res = makeRequest(url, method, requestEntity, responseType, urlVariables);
+			}
+		}		
+		return res;
+	}
+	
+	/** Adds the appropriate Authentication headers to the given HttpEntity. The entity passed in is not modified. New entity
+	 * 		is returned.
+	 * 
+	 * @param entity The HttpEntity to add the request headers
+	 * @return The new HttpEntity with the updated Authentication Headers
+	 */
+	protected HttpEntity<?> addAuthenticationHeaders(final HttpEntity<?> entity) {
+		HttpHeaders headers = entity.getHeaders();		
+		//check if we have an authentication token
+		if (needAuthenticationToken()) {
+			//TODO: hard coded for now, update after credential manager is added
+			headers.add(AFTAuthentication.AFT_AUTH_INIT_HEADER, "testuser|password");
+		}
+		else {
+			headers.add(AFTAuthentication.AFT_AUTH_TOKEN_HEADER, aftToken);
+		}
+
+		return new HttpEntity<Object>(entity.getBody(), headers);
+	}
+	
+	/** Extracts the authentication token from the http response
+	 * 
+	 * @param entity The http response to extract the token from
+	 */
+	
+	private void extractAuthenticationToken(ResponseEntity<?> entity) {
+		String token = entity.getHeaders().getFirst(AFTAuthentication.AFT_AUTH_TOKEN_HEADER);
+		//set it as the authorization token, if it was found, and is not empty
+		if (token != null && !token.isEmpty()) {
+			aftToken = token;
+		}
+	}
+	
+	/** Determines if we need an Authentication token or not
+	 * 
+	 * @return True if we need a token, false otherwise
+	 */
+	private boolean needAuthenticationToken() {
+		return (aftToken == null || aftToken.isEmpty());
+	}
+	
+	/** Invalidates the AFT Token
+	 * 
+	 */
+	private void invalidateAFTToken() {
+		aftToken = null;
+	}
 	
 }
