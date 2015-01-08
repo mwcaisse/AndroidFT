dcd9bce448b9dafcbe956974242f896aa04f185a
Mitchell Caisse <computers1055@gmail.com>
2015-01-07 21:09:50 -0500

    Updated device view to display in rows + columns and wrap

--- aft-servlet/src/main/webapp/js/ViewModels/PFDeviceViewModel.js
+++ aft-servlet/src/main/webapp/js/ViewModels/PFDeviceViewModel.js
@@ -3,9 +3,20 @@
 function DeviceModel(data) {
 	var self = this;
 	
-	self.deviceName = ko.observable(data.deviceName);
-	self.deviceUid = ko.observable(data.deviceUid);
+	/** The id of the device */
+	self.id = ko.observable(data.id);
 	
+	/** The name of the device */
+	self.deviceName = ko.observable(data.deviceName);
+	
+	/** The uid of the device */
+	self.deviceUid = ko.observable(data.deviceUid);
+	/** The image url for the device, compued from the device id */
+	self.deviceImageUrl = ko.computed(function() {
+		return requestRoot + "api/device/image/" + self.id();
+	});
+	
+	/** Whether or not the device is active */
 	self.deviceActive = ko.observable(true);
 }
 
@@ -17,6 +28,18 @@ function PFDeviceViewModel() {
 	
 	/** The list of devices to display */
 	self.devices = ko.observableArray([]);
+	
+	/** The devices in grid format */
+	self.devicesGrid = ko.computed(function() {
+		var data = self.devices();
+		var grid = [];
+		var cols = 5;
+		var rows = Math.ceil(data.length / cols);
+		for (var i = 0; i < rows; i++) {
+			grid.push(data.slice(i * cols, i * cols + cols));
+		}
+		return grid;
+	});
 	
 	
 	self.fetchDevices = function() {
