efef2ce8660b1f43ab47c492f85be6c5f61c27ec
Mitchell Caisse <computers1055@gmail.com>
2015-03-22 14:10:01 -0700

    Cleaned up the request list template + its implementors

--- aft-servlet/src/main/webapp/js/ViewModels/Request/PFRequestListViewModel.js
+++ aft-servlet/src/main/webapp/js/ViewModels/Request/PFRequestListViewModel.js
@@ -1,31 +1,3 @@
-
-/** The model for a Request */
-function RequestModel(data) {	
-	var self = this;	
-	if (data) {		
-		/** The id of this request */
-		self.id = data.id;
-		
-		/** The name of this request */
-		self.requestName = data.requestName;
-		
-		/** The status of this request */
-		self.requestStatus = data.requestStatus;
-		
-		/** The date that this request was updated */
-		self.requestUpdated = data.requestUpdated;
-		
-		self.requestUpdatedText = ko.computed( function() {
-			return parseDate(self.requestUpdated);
-		});
-		
-		/** The name of the device this request belongs to */
-		self.requestDeviceName = data.requestDevice.deviceName;		
-	}
-	
-}
-
-
 /** The view model for displaying a list of all requests
  * 
  * 	@param requestQuery the query used to fetch the requests from the server, without the server root
@@ -49,7 +21,7 @@ function PFRequestListViewModel(requestQuery) {
 			//remove the old requests, and add the new requests
 			self.requests.removeAll();			
 			$.each(data, function(index, value) {
-				self.requests.push(new RequestModel(value));
+				self.requests.push(new BasicRequestModel(value));
 			});			
 		}).fail( function(jqXHR, textStatus, error) {
 			alert("Error fefetching requests! " + textStatus + " : " + error);
